adf31ce2cf78d8e6ecb6d2022e32d1cb8c1faac4
Giovanna Alyssa <gih.alyssa15@gmail.com>
2022-07-29 12:37:22 -0300

    começando alterações em react

---
+++ src/componentes/banner/banner.js
@@ -0,0 +1,14 @@
+import './banner.css'
+
+function Banner() {
+    return(
+      <header className="banner">
+        <img src="/imagens/banner.png"  alt="banner principal da página"/>
+      </header>
+      
+
+    )
+
+}
+
+export default Banner
